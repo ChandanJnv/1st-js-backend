5ba35e02379cc81fd4b065cff93358489779c8dc
Chandan Kumar <cksjnvr@gmail.com>
2024-10-06 11:32:40 +0530

    updating

--- index.js
+++ index.js
@@ -6,13 +6,13 @@ require('dotenv').config()
 const port = process.env.PORT
 
 app.get('/', (req, res) => {
-  res.send('<h1>My 1st Javascript backend for learn deployment</h1>')
+  res.send('SERVER is running...')
 })
 app.get('/name', (req, res) => {
   res.send('<h1>Chandan</h1>')
 })
 app.get('/start', (req, res) => {
-  res.send('this is starting of my project!')
+  res.send('<h1>My 1st Javascript backend for learn deployment</h1>')
 })
 
 app.listen(port, () => {
